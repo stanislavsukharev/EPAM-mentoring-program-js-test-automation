1a29490767df33514bfbca1a1cd84e9181fd2b6b
Stanislav Sukharev <Stanislav_Sukharev@epam.com>
2022-11-17 18:45:13 +0400

    add 30 unit tests

--- test/numbers-validator/unit.spec.js
+++ test/numbers-validator/unit.spec.js
@@ -12,7 +12,15 @@ describe('isNumberEven', () => {
     expect(validator.isNumberEven(4)).to.be.equal(true);
   });
 
-  it('should return false when provided with an even number', () => {
+  it('should return false when provided with a negative odd number', () => {
+    expect(validator.isNumberEven(-3)).to.be.equal(false);
+  });
+
+  it('should return false when provided with an even negative number', () => {
+    expect(validator.isNumberEven(-10)).to.be.equal(true);
+  });
+
+  it('should return false when provided with an odd number', () => {
     expect(validator.isNumberEven(5)).to.be.equal(false);
   });
 
@@ -20,6 +28,40 @@ describe('isNumberEven', () => {
     expect(() => {
       validator.isNumberEven('4');
     }).to.throw('[4] is not of type "Number" it is of type "string"');
+  });
+
+  it('should throw an error when provided a null', () => {
+    expect(() => {
+      validator.isNumberEven(null);
+    }).to.throw('[null] is not of type "Number" it is of type "object"');
+  });
+
+  it('should throw an error when provided a boolean', () => {
+    expect(() => {
+      validator.isNumberEven(true);
+    }).to.throw('[true] is not of type "Number" it is of type "boolean"');
+  });
+
+  it('should throw an error when provided a boolean', () => {
+    expect(() => {
+      validator.isNumberEven(false);
+    }).to.throw('[false] is not of type "Number" it is of type "boolean"');
+  });
+
+  it('should throw an error when provided a undefined', () => {
+    expect(() => {
+      validator.isNumberEven(undefined);
+    }).to.throw('[undefined] is not of type "Number" it is of type "undefined"');
+  });
+
+  it('should throw an error when provided a BigInt', () => {
+    expect(() => {
+      validator.isNumberEven(1234567890123456789012345678901234567890n);
+    }).to.throw('[1234567890123456789012345678901234567890] is not of type "Number" it is of type "bigint"');
+  });
+
+  it('should return false when provided with a floating number', () => {
+    expect(validator.isNumberEven(3)).to.be.equal(false);
   });
 });
 
@@ -37,11 +79,25 @@ describe('getEvenNumbersFromArray', function() {
     ]);
   });
 
-  it('should return false when an array is not an array of numbers', () => {
+  it('should throw an error when an array is not an array of numbers', () => {
     const arrayOfRandomNumbers = [2, 7, 12, '17', 1, 55, 32, 10];
     expect(() => {
       validator.getEvenNumbersFromArray(arrayOfRandomNumbers);
     }).to.throw('[2,7,12,17,1,55,32,10] is not an array of "Numbers"');
+  });
+
+  it('should throw an error when an array is not an array of numbers', () => {
+    const arrayOfRandomNumbers = ['24', 'eleven', 'cat', null];
+    expect(() => {
+      validator.getEvenNumbersFromArray(arrayOfRandomNumbers);
+    }).to.throw('[24,eleven,cat,] is not an array of "Numbers"');
+  });
+
+  it('should throw an error when an array is not an array of numbers', () => {
+    const arrayOfRandomNumbers = [1234567890123456789012345678901234567890n, false, undefined];
+    expect(() => {
+      validator.getEvenNumbersFromArray(arrayOfRandomNumbers);
+    }).to.throw('[1234567890123456789012345678901234567890,false,] is not an array of "Numbers"');
   });
 });
 
@@ -51,9 +107,24 @@ describe('isAllNumbers', () => {
     validator = new NumbersValidator();
   });
 
-  it('should return true when provided with an integer', () => {
+  it('should return true when provided with a number', () => {
     const arrayOfNumbers = [4, 5, 19, 8];
     expect(validator.isAllNumbers(arrayOfNumbers)).to.equal(true);
+  });
+
+  it('should return true when provided with a negative number', () => {
+    const arrayOfNumbers = [-100, -43, -54, -18];
+    expect(validator.isAllNumbers(arrayOfNumbers)).to.equal(true);
+  });
+
+  it('should return false when provided with a string', () => {
+    const arrayOfNumbers = ['100', 'cat', 34, ''];
+    expect(validator.isAllNumbers(arrayOfNumbers)).to.equal(false);
+  });
+
+  it('should return false when provided data not numbers', () => {
+    const arrayOfNumbers = [null, true, Infinity, undefined];
+    expect(validator.isAllNumbers(arrayOfNumbers)).to.equal(false);
   });
 
   it('should return false when provided not an array', () => {
@@ -61,6 +132,20 @@ describe('isAllNumbers', () => {
     expect(() => {
       validator.isAllNumbers(arrayOfNumbers);
     }).to.throw('[1] is not an array');
+  });
+
+  it('should return false when provided not an array', () => {
+    const arrayOfNumbers = true;
+    expect(() => {
+      validator.isAllNumbers(arrayOfNumbers);
+    }).to.throw('[true] is not an array');
+  });
+
+  it('should return false when provided not an array', () => {
+    const arrayOfNumbers = {type: 'Fiat'};
+    expect(() => {
+      validator.isAllNumbers(arrayOfNumbers);
+    }).to.throw('[[object Object]] is not an array');
   });
 });
 
@@ -74,9 +159,43 @@ describe('isInteger', () => {
     expect(validator.isInteger(4)).to.be.equal(true);
   });
 
+  it('should return true when provided with an negative integer', () => {
+    expect(validator.isInteger(-50)).to.be.equal(true);
+  });
+
   it('should throw an error when provided data not a number', () => {
     expect(() => {
       validator.isInteger(null);
     }).to.throw('[null] is not a number');
+  });
+
+  it('should throw an error when provided data not a number', () => {
+    expect(() => {
+      validator.isInteger('meaw');
+    }).to.throw('[meaw] is not a number');
+  });
+
+  it('should throw an error when provided data not a number', () => {
+    expect(() => {
+      validator.isInteger([-100, -43, -54, -18]);
+    }).to.throw('[-100,-43,-54,-18] is not a number');
+  });
+
+  it('should throw an error when provided data not a number', () => {
+    expect(() => {
+      validator.isInteger({type: 'Whale'});
+    }).to.throw('[[object Object]] is not a number');
+  });
+
+  it('should throw an error when provided data not a number', () => {
+    expect(() => {
+      validator.isInteger(1234567890123456789012345678901234567890n);
+    }).to.throw('[1234567890123456789012345678901234567890] is not a number');
+  });
+
+  it('should throw an error when provided data not a number', () => {
+    expect(() => {
+      validator.isInteger(false);
+    }).to.throw('[false] is not a number');
   });
 });
